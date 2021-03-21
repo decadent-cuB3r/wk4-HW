990f89f12ac1425e462472b172842244449fbec2
decadent-cuB3r <cub3r.ezra.yu@gmail.com>
2021-03-22 03:51:41 +0800

    product-point

--- src/components/ProductDetail.js
+++ src/components/ProductDetail.js
@@ -7,6 +7,7 @@ const { Option } = Select;
 
 function ProductDetail({ product }) {
   const [qty, setQty] = useState(product.countInStock > 0 ? 1 : 0);
+  const [point, setPoint] = useState(product.point[0]);
 
   return (
     <Row gutter={[32, 32]}>
@@ -25,35 +26,44 @@ function ProductDetail({ product }) {
             <p className="product-status">
               Status: {product.countInStock > 0 ? "In Stock" : "Unavailable."}
             </p>
-            <p className="product-qty">
-              Qty: {"   "}
-              <Select
-                defaultValue={qty}
-                className="select-style"
-                onChange={(val) => setQty(val)}
-              >
-                {[...Array(product.countInStock).keys()].map((x) => (
-                  <Option key={x + 1} value={x + 1}>
-                    {x + 1}
-                  </Option>
-                ))}
-              </Select>
-            </p>
-            <p className="product-test">
-              Qty: {"   "}
-              <Select
-                defaultValue={qty}
-                className="select-style"
-                onChange={(val) => setQty(val)}
-              >
-                {[...Array(product.countInStock).keys()].map((x) => (
-                  <Option key={x + 1} value={x + 1}>
-                    {x + 1}
-                  </Option>
-                ))}
-              </Select>
-            </p>
-            <p className="product-qty">Total Price: {product.price * qty}</p>
+            <Row>
+              <Col lg={{ span: 8 }}>
+                <p className="product-qty">
+                  Qty: {"  "}
+                  <Select
+                    defaultValue={qty}
+                    className="select-style"
+                    onChange={(val) => setQty(val)}
+                  >
+                    {[...Array(product.countInStock).keys()].map((x) => (
+                      <Option key={x + 1} value={x + 1}>
+                        {x + 1}
+                      </Option>
+                    ))}
+                  </Select>
+                </p>
+              </Col>
+              <p className="product-test">
+                點和: {"   "}
+                <Select
+                  defaultValue={point}
+                  className="select-style"
+                  onChange={(val) => setPoint(val)}
+                >
+                  {[...Array(product.point.length).keys()].map((x) => (
+                    <Option value={product.point[x]}>{product.point[x]}</Option>
+                  ))}
+                </Select>
+              </p>
+            </Row>
+            <Row>
+              <Col lg={{ span:8 }}>
+                <p className="product-qty">
+                  Total Price: {product.price * qty}
+                </p>
+              </Col>
+              <p className="product-point">Total Points: { point }</p>
+            </Row>
             <AddToCart />
           </div>
         </div>
