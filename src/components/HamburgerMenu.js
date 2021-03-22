32579a2c096c9acde9f0dc303742fd1eaa87c82d
decadent-cuB3r <cub3r.ezra.yu@gmail.com>
2021-03-22 20:31:56 +0800

    Hamburger Menu

---
+++ src/components/HamburgerMenu.js
@@ -0,0 +1,11 @@
+export default function HamburgerMenu({ onClick, isOnTouch }) {
+    return (
+        <footer className="footer">
+            <hr className="hr-footer-line" />
+            <p
+                className="text-gray">
+                Copyright.
+            </p>
+        </footer>           
+    );
+}
